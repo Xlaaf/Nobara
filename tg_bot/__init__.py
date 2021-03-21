697000e15b359e73135bfb38e1d24a4158231f67
Ryomen-Sukuna <ryomensukuna83@gmail.com>
2021-03-21 17:07:54 +0700

    reedit init
    Signed-off-by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -91,11 +91,17 @@ if spamwatch_api is None:
     sw = None
     log.warning("SpamWatch API key is missing! Check your config.ini")
 else:
+	sw = spamwatch.Client(spamwatch_api)
+
+REDIS_URL = Config.REDIS_URI
+REDIS = StrictRedis.from_url(REDIS_URL,decode_responses=True)
+
     try:
-        sw = spamwatch.Client(spamwatch_api)
-    except:
-        sw = None
-        log.warning("Can't connect to SpamWatch!")
+        REDIS.ping()
+    LOGGER.info("Your redis server is now alive!")
+    
+except BaseException:
+    raise Exception("Your redis server is not alive, please check again.")
 
 updater = tg.Updater(TOKEN, workers=min(32, os.cpu_count() + 4), request_kwargs={"read_timeout": 10, "connect_timeout": 10})
 telethn = TelegramClient(MemorySession(), APP_ID, API_HASH)
