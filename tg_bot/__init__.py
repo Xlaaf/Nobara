7cb1186368d931307696d76caa541b3890584f09
Ryomen-Sukuna <ryomensukuna83@gmail.com>
2021-03-21 17:16:54 +0700

    fixing init
    Signed-off-by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -98,7 +98,7 @@ REDIS = StrictRedis.from_url(REDIS_URL,decode_responses=True)
 
     try:
         REDIS.ping()
-    LOGGER.info("Your redis server is now alive!")
+        log.warning("Your redis server is now alive!")
     
 except BaseException:
     raise Exception("Your redis server is not alive, please check again.")
