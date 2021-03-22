950c0f7de876b9f6b553ae75dfdbe9f97eda9dd0
Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-03-22 16:02:41 +0700

    update afk
    Signed-off by: Ryomen-Sukuna <ryomensukuna83@gmail.com>

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -1,18 +1,18 @@
 import random
 import html
-import time
+from datetime import datetime
 import humanize
 
-from telegram import Update, MessageEntity
-from telegram.ext import Filters, CallbackContext, MessageHandler
-from telegram.error import BadRequest
-from tg_bot import dispatcher
-from tg_bot.modules.disable import (
+from SaitamaRobot import dispatcher
+from SaitamaRobot.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from tg_bot.modules.sql import afk_sql as sql
-from tg_bot.modules.users import get_user_id
+from SaitamaRobot.modules.sql import afk_sql as sql
+from SaitamaRobot.modules.users import get_user_id
+from telegram import MessageEntity, Update
+from telegram.error import BadRequest
+from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
 
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
@@ -45,22 +45,33 @@ def afk(update: Update, context: CallbackContext):
     except BadRequest:
         pass
 
+
 def no_longer_afk(update: Update, context: CallbackContext):
     user = update.effective_user
     message = update.effective_message
+
     if not user:  # ignore channels
         return
 
-    afk_time = sql.get_afk_time(user.id)
-    afk_since = get_readable_time((time.time() - float(afk_time)))
     res = sql.rm_afk(user.id)
     if res:
-        if message.new_chat_members:  #dont say msg
+        if message.new_chat_members:  # dont say msg
             return
         firstname = update.effective_user.first_name
         try:
-            message.reply_text(
-                "{} is no longer AFK!\nTime you were AFK for: {}".format(firstname, afk_since))
+            options = [
+                "{} is here!",
+                "{} is back!",
+                "{} is now in the chat!",
+                "{} is awake!",
+                "{} is back online!",
+                "{} is finally here!",
+                "Welcome back! {}",
+                "Where is {}?\nIn the chat!",
+            ]
+            chosen_option = random.choice(options)
+            update.effective_message.reply_text(
+                chosen_option.format(firstname))
         except:
             return
 
@@ -87,31 +98,25 @@ def reply_afk(update: Update, context: CallbackContext):
                     return
                 chk_users.append(user_id)
 
-            if ent.type == MessageEntity.MENTION:
-                user_id = get_user_id(
-                    message.text[ent.offset : ent.offset + ent.length]
-                )
-                if not user_id:
-                    # Should never happen, since for a user to become AFK they must have spoken. Maybe changed username?
-                    return
-
-                if user_id in chk_users:
-                    return
-                chk_users.append(user_id)
-
-                try:
-                    chat = bot.get_chat(user_id)
-                except BadRequest:
-                    print(
-                        "Error: Could not fetch userid {} for AFK module".format(
-                            user_id
-                        )
-                    )
-                    return
-                fst_name = chat.first_name
-
-            else:
+            if ent.type != MessageEntity.MENTION:
                 return
+
+            user_id = get_user_id(
+                message.text[ent.offset: ent.offset + ent.length])
+            if not user_id:
+                # Should never happen, since for a user to become AFK they must have spoken. Maybe changed username?
+                return
+
+            if user_id in chk_users:
+                return
+            chk_users.append(user_id)
+
+            try:
+                chat = bot.get_chat(user_id)
+            except BadRequest:
+                print("Error: Could not fetch userid {} for AFK module".format(user_id))
+                return
+            fst_name = chat.first_name
 
             check_afk(update, context, user_id, fst_name, userc_id)
 
@@ -121,51 +126,28 @@ def reply_afk(update: Update, context: CallbackContext):
         check_afk(update, context, user_id, fst_name, userc_id)
 
 
-def check_afk(update, context, user_id, fst_name, userc_id):
+def check_afk(update: Update, context: CallbackContext, user_id: int, fst_name: str, userc_id: int):
     if sql.is_afk(user_id):
         user = sql.check_afk_status(user_id)
-        afk_time = sql.get_afk_time(user_id)
-        afk_since = get_readable_time((time.time() - afk_time))
+
+        if int(userc_id) == int(user_id):
+            return
+
+        time = humanize.naturaldelta(datetime.now() - user.time)
+
         if not user.reason:
-            if int(userc_id) == int(user_id):
-                return
-            res = "{} is afk since {}".format(fst_name, afk_since)
-            update.effective_message.reply_text(res)
-        else:
-            if int(userc_id) == int(user_id):
-                return
-            res = "{} is afk since {}\nReason: {}".format(
-                fst_name, afk_since, user.reason
+            res = "{} is afk.\nLast seen {} ago.".format(
+                fst_name,
+                time
             )
             update.effective_message.reply_text(res)
-
-
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-
-    while count < 4:
-        count += 1
-        if count < 3:
-            remainder, result = divmod(seconds, 60)
         else:
-            remainder, result = divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-
-    return ping_time
+            res = "{} is AFK.\nReason: <code>{}</code>\nLast seen <code>{}</code> ago.".format(
+                html.escape(fst_name),
+                html.escape(user.reason),
+                time
+            )
+            update.effective_message.reply_text(res, parse_mode="html")
 
 
 def __gdpr__(user_id):
