9bdc7f42aa291d6b1964d3f31d484cdeb4353a2c
Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-03-22 11:30:10 +0700

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -41,7 +41,7 @@ def no_longer_afk(update: Update, context: CallbackContext):
     if not user:  # ignore channels
         return
 
-    afk_time = sql.get_afk_time(user_id)
+    afk_time = sql.get_afk_time(user.id)
     afk_since = get_readable_time((time.time() - afk_time))
     res = sql.rm_afk(user.id)
     if res:
